a9d3ac44add370784e8620c251df047d63f0e545
elias <elias.canche@asore.net>
2016-11-03 16:36:26 -0600

    resetear formulario cuando se crea un empleado

--- modules/employees/client/controllers/employees.client.controller.js
+++ modules/employees/client/controllers/employees.client.controller.js
@@ -5,7 +5,7 @@
 	angular.module('employees').controller('EmployeesController', ['$scope', '$log', '$uibModal', '$location', '$stateParams', '$http', 'EmployeesService',
 		function ($scope, $log, $uibModal, $location, $stateParams, $http, EmployeesService) {
 			var $ctrl = this;
-
+		
 			$scope.successTextAlert = "Some content";
 			$scope.showSuccessAlert = false;
 			$scope.employee = {};
@@ -15,11 +15,14 @@
 				$scope[value] = !$scope[value];
 			};
 
+			console.log($scope.employee);
+
 			$scope.submit = function(form){
 				EmployeesService.createEmployee($scope.employee).then(function(response){
 					$scope.successTextAlert = 'Se ha creado al empleado correctamente!';
 					$scope.showSuccessAlert = true;
-					$location.path('/employees/' + response.data._id);
+					$scope.employee = {};
+					$scope.formEmployee.$setUntouched();
 				}).catch(function(err) {
 					console.log("err", err);
 				});
