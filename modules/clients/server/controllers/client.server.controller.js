4b437785f8698cbff1d90e9a0fac7f05e1d426e5
André <pumpitupnx2@hotmail.com>
2016-11-13 19:48:04 -0600

    Created client model

---
+++ modules/clients/server/controllers/client.server.controller.js
@@ -0,0 +1,111 @@
+/**
+ * Created by Andre on 13/11/2016.
+ */
+'use strict';
+
+var path = require('path');
+var mongoose = require("mongoose");
+var Client = mongoose.model("Client");
+var errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+
+exports.create = function (req, res) {
+    var client = new Client(req.body);
+
+    client.save(function (err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.json(client);
+        }
+
+    });
+};
+
+
+exports.update = function (req, res) {
+    var client = Client.findById(req.params.clientId).exec(function (err, client) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            if (!client) {
+                return res.status(404).send({
+                    message: "No se encontro el cliente"
+                });
+            }
+
+            client.name = req.body.name;
+            client.contact = req.body.contact;
+            client.address = req.body.address;
+            client.taxId = req.body.taxId;
+            client.phone = req.body.phone;
+            client.email = req.body.email;
+            client.url = req.body.url;
+
+            client.save(function (err) {
+                if (err) {
+                    return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                    });
+                } else {
+                    res.json(client);
+                }
+
+            });
+
+
+        }
+    })
+};
+
+exports.list = function (req, res) {
+    Client.find({deleteAt: null}, function (err, clients) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(clients);
+        }
+    });
+};
+
+
+exports.read = function (req, res) {
+    Client.find(req.params.clientId, function (err, client) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            if (!client) {
+                return res.status(404).send({
+                    message: "No se encontro el cliente"
+                });
+            }
+
+            res.json(client);
+        }
+    });
+};
+
+
+exports.delete = function (req, res) {
+    Client.findById(req.params.clientId).exec(function (err, client) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            if (!client) {
+                return res.status(404).send({
+                    message: "No se encontro el cliente"
+                });
+            }
+
+            client.deleteAt = Date.now();
+        }
+
+    });
+};
---
+++ modules/clients/server/models/client.server.model.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Andre on 13/11/2016.
+ */
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid Email"];
+
+var clientSchema = new Schema({
+        name: String,
+        contact: {
+            name: String,
+            officePhone: String,
+            cellPhone: String,
+            email: String
+        },
+        address: {
+            city: String,
+            state: String,
+            country: String,
+            zipCode: String,
+            street: String,
+            colony: String,
+            lot: String,
+            crosses: String
+        },
+        taxId: String,
+        phone: String,
+        email: {type: String, required: "Write your email", match: email_match},
+        url: String,
+        workTeams: [{type: mongoose.Schema.Types.ObjectId, ref: 'WorkTeam'}],
+        purchaseOrders: [{type: mongoose.Schema.Types.ObjectId, ref: 'PurchaseOrder'}]
+    },
+    {
+        timestamps: {createdAt: 'created_at', updatedAt: 'updated_at', deleteAt: 'delete_at'}
+    });
+
+mongoose.model("Client", clientSchema);
