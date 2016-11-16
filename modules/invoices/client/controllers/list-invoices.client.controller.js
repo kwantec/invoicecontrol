0149c1da9f77dd47169d946a2f6673b7537a83f1
Angel Caamal <acaamal@kwantec.com>
2016-11-15 19:13:59 -0600

    Agregado el subobjeto al Schema de Invoices, ademas de corrección de errores.

--- modules/invoices/client/controllers/list-invoices.client.controller.js
+++ modules/invoices/client/controllers/list-invoices.client.controller.js
@@ -5,11 +5,38 @@
     .module('invoices')
     .controller('InvoicesListController', InvoicesListController);
 
-  InvoicesListController.$inject = ['InvoicesService'];
+  InvoicesListController.$inject = ['InvoicesService', '$mdDialog'];
 
-  function InvoicesListController(InvoicesService) {
+  function InvoicesListController(InvoicesService, $mdDialog) {
     var vm = this;
 
     vm.invoices = InvoicesService.query();
+
+      vm.showPrompt = function() {
+          var confirm = $mdDialog.prompt()
+              .title('Escoge un timesheet')
+              .textContent('Antes de empezar con tu carga nos gustaría saber tu nombre')
+              .placeholder('Tú nombre')
+              .ariaLabel('Tú nombre')
+              .ok('¡Listo!');
+
+          $mdDialog.show(confirm).then(function (result) {
+              if (result !== undefined) {
+                  vm.student = student;
+                  vm.student.name = result;
+                  vm.student.$save(vm.student, function (response) {
+                      console.log(response);
+                      vm.student._id = response._id;
+                      vm.student.periods = [];
+                  });
+                  console.log(vm.student);
+              } else {
+                  showPrompt();
+              }
+          }, function () {
+              showPrompt();
+          });
+      }
+
   }
 }());
