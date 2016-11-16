870123d18e3b056bb0031236af961c395738e3ac
Angel Caamal <acaamal@kwantec.com>
2016-11-16 10:00:59 -0600

    Agregado el subobjeto al Schema de Invoices, ademas de corrección de errores.

--- modules/invoices/client/controllers/list-invoices.client.controller.js
+++ modules/invoices/client/controllers/list-invoices.client.controller.js
@@ -1,42 +1,35 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('invoices')
-    .controller('InvoicesListController', InvoicesListController);
+    angular
+        .module('invoices')
+        .controller('InvoicesListController', InvoicesListController);
 
-  InvoicesListController.$inject = ['InvoicesService', '$mdDialog'];
+    InvoicesListController.$inject = ['InvoicesService', '$mdDialog'];
 
-  function InvoicesListController(InvoicesService, $mdDialog) {
-    var vm = this;
+    function InvoicesListController(InvoicesService, $mdDialog) {
+        var vm = this;
 
-    vm.invoices = InvoicesService.query();
+        vm.invoices = InvoicesService.query();
 
-      vm.showPrompt = function() {
-          var confirm = $mdDialog.prompt()
-              .title('Escoge un timesheet')
-              .textContent('Antes de empezar con tu carga nos gustaría saber tu nombre')
-              .placeholder('Tú nombre')
-              .ariaLabel('Tú nombre')
-              .ok('¡Listo!');
+        vm.showPrompt = function () {
+            var confirm = $mdDialog.prompt()
+                .title('Escoge un timesheet')
+                .textContent('Antes de empezar con tu carga nos gustaría saber tu nombre')
+                .placeholder('Tú nombre')
+                .ariaLabel('Tú nombre')
+                .ok('¡Listo!');
 
-          $mdDialog.show(confirm).then(function (result) {
-              if (result !== undefined) {
-                  vm.student = student;
-                  vm.student.name = result;
-                  vm.student.$save(vm.student, function (response) {
-                      console.log(response);
-                      vm.student._id = response._id;
-                      vm.student.periods = [];
-                  });
-                  console.log(vm.student);
-              } else {
-                  showPrompt();
-              }
-          }, function () {
-              showPrompt();
-          });
-      }
+            $mdDialog.show(confirm).then(function (result) {
+                if (result !== undefined) {
 
-  }
+                } else {
+                    vm.showPrompt();
+                }
+            }, function () {
+                vm.showPrompt();
+            });
+        };
+
+    }
 }());
