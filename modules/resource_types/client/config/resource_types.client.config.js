abede203f3226b5873c44f6619c28058edef0164
Felix Diaz <pixcompu@outlook.com>
2016-11-17 08:21:30 -0600

    Adding the feature and general client structure

---
+++ modules/resource_types/client/config/resource_types.client.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Configuring the ResourceTypes module
+angular.module('resourceTypes').run(['Menus',
+  function (Menus) {
+    // Add the articles dropdown item
+    Menus.addMenuItem('topbar', {
+      title: 'Tipos de Recurso',
+      state: 'resource-types',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    Menus.addSubMenuItem('topbar', 'resource-types', {
+      title: 'Lista de Tipos de Recurso',
+      state: 'resource-types.list'
+    });
+
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'resource-types', {
+      title: 'Crear un Tipo de Recurso',
+      state: 'resource-types.create',
+      roles: ['user']
+    });
+  }
+]);
---
+++ modules/resource_types/client/config/resource_types.client.routes.js
@@ -0,0 +1,39 @@
+(function(){
+    'use strict';
+    angular
+        .module('resourceTypes')
+        .config(routeConfig);
+
+    routeConfig.$inject = ['$stateProvider'];
+    function routeConfig($stateProvider) {
+        $stateProvider
+            .state('resource-types', {
+                abstract: true,
+                url: '/resource-types',
+                template: '<ui-view/>'
+            })
+            .state('resource-types.list', {
+                url: '',
+                templateUrl: 'modules/resource_types/client/views/list-resource-type.client.view.html'
+            })
+            .state('resource-types.create', {
+                url: '/create',
+                templateUrl: 'modules/resource_types/client/views/create-resource-type.client.view.html',
+                data: {
+                    roles: ['user', 'admin']
+                }
+            })
+            .state('resource-types.view', {
+                url: '/:resourceTypeId',
+                templateUrl: 'modules/resource_types/client/views/view-resource-type.client.view.html'
+            })
+            .state('resource-types.edit', {
+                url: '/:resourceTypeId/edit',
+                templateUrl: 'modules/resource_types/client/views/edit-resource-type.client.view.html',
+                data: {
+                    roles: ['user', 'admin']
+                }
+            });
+    }
+})();
+
