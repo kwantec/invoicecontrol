152dc6e791c486c1a1ada8270d38bcadd75569b6
Joshua E. Flores Tuz <josshft@gmail.com>
2016-11-24 19:27:06 -0600

    Terminando integracion de Angular Material

--- modules/invoices/client/controllers/list-invoices.client.controller.js
+++ modules/invoices/client/controllers/list-invoices.client.controller.js
@@ -5,29 +5,35 @@
         .module('invoices')
         .controller('InvoicesListController', InvoicesListController);
 
-    InvoicesListController.$inject = ['InvoicesService', '$mdDialog'];
+    InvoicesListController.$inject = ['InvoicesService', '$mdDialog', '$scope'];
 
-    function InvoicesListController(InvoicesService, $mdDialog) {
+    function InvoicesListController(InvoicesService, $mdDialog, $scope) {
         var vm = this;
 
         vm.invoices = InvoicesService.query();
 
-        vm.showPrompt = function () {
-            var confirm = $mdDialog.prompt()
-                .title('Escoge un timesheet')
-                .textContent('Antes de empezar con tu carga nos gustaría saber tu nombre')
-                .placeholder('Tú nombre')
-                .ariaLabel('Tú nombre')
-                .ok('¡Listo!');
-
-            $mdDialog.show(confirm).then(function (result) {
-                if (result !== undefined) {
-
-                } else {
-                }
-            }, function () {
+        vm.showPrompt = function ($event) {
+            var parentEl = angular.element(document.body);
+            $mdDialog.show({
+                parent: parentEl,
+                targetEvent: $event,
+                templateUrl: 'modules/invoices/client/views/modal.pick.timesheet.html',
+                locals: {},
+                controller: DialogController
             });
         };
+
+        function DialogController($scope, $mdDialog) {
+            $scope.closeDialog = function () {
+                $mdDialog.hide();
+            };
+
+            $scope.hasChanged = function () {
+                console.log($scope.selected);
+            };
+
+            $scope.fieldTable = [1,2,3,4];
+        }
 
     }
 }());
