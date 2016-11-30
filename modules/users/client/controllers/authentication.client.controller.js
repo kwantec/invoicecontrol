adaa8cf686e59a4a353b8547acceb4931a433ac6
Izanami Chin <chinparra.alex@gmail.com>
2016-11-29 18:59:38 -0600

    inject the permissions in the usergroup.

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'PasswordValidator',
-  function ($scope, $state, $http, $location, $window, Authentication, PasswordValidator) {
+angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'PasswordValidator','PermissionsService',
+  function ($scope, $state, $http, $location, $window, Authentication, PasswordValidator, PermissionsService) {
     $scope.authentication = Authentication;
     $scope.popoverMsg = PasswordValidator.getPopoverMsg();
 
@@ -25,7 +25,10 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
       $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
-
+        //add the permissions
+        PermissionsService.getPermissionsByUser($scope.authentication.user).success(function (data) {
+          $scope.authentication.user.permissions = data;
+        });
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'home', $state.previous.params);
       }).error(function (response) {
@@ -45,7 +48,10 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
       $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
-
+        //add the permissions
+        PermissionsService.getPermissionsByUser($scope.authentication.user).success(function (data) {
+          $scope.authentication.user.permissions = data;
+        });
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'home', $state.previous.params);
       }).error(function (response) {
