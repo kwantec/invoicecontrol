2521e9d53726c32a3527feec50c95b3039adf43d
Joshua E. Flores Tuz <josshft@gmail.com>
2016-12-06 14:55:38 -0600

    Imprimir y exportar a PDF Listo.

--- modules/invoices/client/controllers/invoices.client.controller.js
+++ modules/invoices/client/controllers/invoices.client.controller.js
@@ -1,87 +1,95 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  // Invoices controller
-  angular
-    .module('invoices')
-    .controller('InvoicesController', InvoicesController);
+    // Invoices controller
+    angular
+        .module('invoices')
+        .controller('InvoicesController', InvoicesController);
 
-  InvoicesController.$inject = ['$scope', '$state', '$window', 'Authentication', 'invoiceResolve', 'timesheetResolve', '$sce'];
+    InvoicesController.$inject = ['$scope', '$state', '$window', 'Authentication', 'invoiceResolve', 'timesheetResolve', '$sce'];
 
-  function InvoicesController ($scope, $state, $window, Authentication, invoice, timesheet, $sce) {
+    function InvoicesController($scope, $state, $window, Authentication, invoice, timesheet, $sce) {
 
-    var vm = this;
+        var vm = this;
 
-    vm.authentication = Authentication;
-    vm.invoice = invoice;
-    vm.error = null;
-    vm.form = {};
-    vm.remove = remove;
-    vm.save = save;
-    vm.hasChanged = hasChanged;
+        vm.authentication = Authentication;
+        vm.invoice = invoice;
+        vm.error = null;
+        vm.form = {};
+        vm.remove = remove;
+        vm.save = save;
+        vm.hasChanged = hasChanged;
+        vm.printPage = printPage;
 
-    vm.invoice.teamName = timesheet.teamName;
-    vm.invoice.workDaysInPeriod= timesheet.workDaysInPeriod;
-    vm.invoice.workDaysInMonth = timesheet.workDaysInMonth;
-    vm.invoice.finishDate = new Date(timesheet.finishDate);
-    vm.invoice.startDate =  new Date(timesheet.startDate);
-    vm.invoice.employees = timesheet.employees;    
+        vm.invoice.teamName = timesheet.teamName;
+        vm.invoice.workDaysInPeriod = timesheet.workDaysInPeriod;
+        vm.invoice.workDaysInMonth = timesheet.workDaysInMonth;
+        vm.invoice.finishDate = new Date(timesheet.finishDate);
+        vm.invoice.startDate = new Date(timesheet.startDate);
+        vm.invoice.employees = timesheet.employees;
 
-    
 
-    // Remove existing Invoice
-    function remove() {
-      if ($window.confirm('Are you sure you want to delete?')) {
-        vm.invoice.$remove($state.go('invoices.list'));
-      }
+        // Remove existing Invoice
+        function remove() {
+            if ($window.confirm('Are you sure you want to delete?')) {
+                vm.invoice.$remove($state.go('invoices.list'));
+            }
+        }
+
+        // Save Invoice
+        function save(isValid) {
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'vm.form.invoiceForm');
+                return false;
+            }
+
+            // TODO: move create/update logic to service
+            if (vm.invoice._id) {
+                vm.invoice.$update(successCallback, errorCallback);
+            } else {
+                vm.invoice.$save(successCallback, errorCallback);
+            }
+
+            function successCallback(res) {
+                $state.go('invoices.view', {
+                    invoiceId: res._id
+                });
+            }
+
+            function errorCallback(res) {
+                vm.error = res.data.message;
+            }
+        }
+
+        function hasChanged() {
+
+            vm.invoice.totalDiscount = 0;
+            vm.invoice.totalChargesDiscount = 0;
+            vm.invoice.totalChargesNoDiscount = 0;
+            angular.forEach(vm.invoice.employees, function (value, key) {
+                //console.log(key + ': ' + value.billing.currentPeriodCharges);
+                vm.invoice.totalChargesNoDiscount += value.billing.currentPeriodCharges;
+                vm.invoice.totalDiscount += value.billing.discount;
+                value.billing.totalPeriodCharges =
+                    value.billing.currentPeriodCharges - (value.billing.currentPeriodCharges * (value.billing.discount / 100));
+                vm.invoice.totalChargesDiscount += value.billing.totalPeriodCharges;
+            });
+        }
+
+        function printPage () {
+            console.log('PRINTING');
+            vm.originalBody = document.body.innerHTML;
+            document.body.innerHTML = document.getElementById('invoice').innerHTML;
+            window.print();
+            document.body.innerHTML = vm.originalBody;
+        }
+
     }
 
-    // Save Invoice
-    function save(isValid) {
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'vm.form.invoiceForm');
-        return false;
-      }
-
-      // TODO: move create/update logic to service
-      if (vm.invoice._id) {
-        vm.invoice.$update(successCallback, errorCallback);
-      } else {
-        vm.invoice.$save(successCallback, errorCallback);
-      }
-
-      function successCallback(res) {
-        $state.go('invoices.view', {
-          invoiceId: res._id
-        });
-      }
-
-      function errorCallback(res) {
-        vm.error = res.data.message;
-      }
-    }
-
-    function hasChanged(){
-      
-      vm.invoice.totalDiscount = 0;
-      vm.invoice.totalChargesDiscount = 0;
-      vm.invoice.totalChargesNoDiscount = 0;
-      angular.forEach(vm.invoice.employees, function(value, key){
-        //console.log(key + ': ' + value.billing.currentPeriodCharges);
-        vm.invoice.totalChargesNoDiscount += value.billing.currentPeriodCharges;
-        vm.invoice.totalDiscount += value.billing.discount;
-        value.billing.totalPeriodCharges = 
-          value.billing.currentPeriodCharges - (value.billing.currentPeriodCharges*(value.billing.discount/100));
-        vm.invoice.totalChargesDiscount += value.billing.totalPeriodCharges;
-      });
-    }
-
-  }
-
-  /*$scope.hasChanged = function () {
-    angular.forEach(vm.invoice.employees, function(value, key){
+    /*$scope.hasChanged = function () {
+     angular.forEach(vm.invoice.employees, function(value, key){
      console.log(key + ': ' + value.billing.currentPeriodCharges);
-    });
-  };*/
+     });
+     };*/
 
 }());
